3232d05566cf008b377e58753c0137c833c07db0
Manidgyf <105136680+Manidgyf@users.noreply.github.com>
2025-07-15 01:30:43 +0530

    Array 14-07-25 NextPermutation

---
+++ src/Arrays/NextPermutation.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+class NextPermutation {
+    public static void main(String args[])
+    {
+        int arr[] = new int[]{1,2,3};
+        nextPermutation(arr);
+    }
+    public void nextPermutation(int[] nums) {
+        int n = nums.length - 1;
+        int ind = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        if (ind == -1) {
+            reverseArray(nums, 0, n);
+        } else {
+            for (int i = n; i > ind; i--) {
+                if (nums[i] > nums[ind]) {
+                    int temp = nums[ind];
+                    nums[ind] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+            }
+            reverseArray(nums, ind + 1, n);
+        }
+    }
+
+    public void reverseArray(int arr[], int start, int end) {
+        int i = start;
+        int j = end;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
